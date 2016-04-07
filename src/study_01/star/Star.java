6d7e24283e7d305833f6004f61921e234d5abc20
TalliCha <TalliCha@DESKTOP-126K61L>
2016-04-07 22:09:25 +0900

    create Star.java

---
+++ src/study_01/star/Star.java
@@ -0,0 +1,35 @@
+package study_01.star;
+
+public class Star {
+
+	public static void main(String[] args) {
+		triangle(); // 삼각형
+		Inverted_triangle(); // 역삼각형
+
+		diamond(); // 다이아몬드
+		Sandglass(); // 모래시계
+
+		rectangle(); // 직각 삼각형
+	}
+
+	private static void triangle() {
+
+	}
+
+	private static void Inverted_triangle() {
+
+	}
+
+	private static void diamond() {
+
+	}
+
+	private static void Sandglass() {
+
+	}
+
+	private static void rectangle() {
+
+	}
+
+}
