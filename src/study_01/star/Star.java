4e2d3d4b74880967c6c50b9aed73f7ba111e1d1c
TalliCha <TalliCha@DESKTOP-126K61L>
2016-04-08 03:10:15 +0900

    Star.java 메서드 기능 작성 삼각형 역삼각형
    다이아몬드
    모래시계
    
    직삼각형

--- src/study_01/star/Star.java
+++ src/study_01/star/Star.java
@@ -3,33 +3,87 @@ package study_01.star;
 public class Star {
 
 	public static void main(String[] args) {
-		triangle(); // 삼각형
-		Inverted_triangle(); // 역삼각형
+		triangle(7); // 삼각형
+		Inverted_triangle(7); // 역삼각형
 
-		diamond(); // 다이아몬드
-		Sandglass(); // 모래시계
+		diamond(7); // 다이아몬드
+		Sandglass(7); // 모래시계
 
-		rectangle(); // 직각 삼각형
+		rectangle(7); // 직각 삼각형
 	}
 
-	private static void triangle() {
-
+	/**
+	 * @param size
+	 */
+	private static void triangle(int size) {
+		System.out.println("###삼각형###");
+		for (int row = 0; row < size / 2 + 1; row++) {
+			for (int col = 0; col < size; col++) {
+				System.out.print((size / 2 - row <= col && col <= size / 2 + row) ? "*" : " ");
+			}
+			System.out.println();
+		}
 	}
 
-	private static void Inverted_triangle() {
-
+	/**
+	 * @param size
+	 */
+	private static void Inverted_triangle(int size) {
+		System.out.println("###역삼각형###");
+		for (int row = 0; row < size / 2 + 1; row++) {
+			for (int col = 0; col < size; col++) {
+				System.out.print((row <= col && col <= size - row - 1) ? "*" : " ");
+			}
+			System.out.println();
+		}
 	}
 
-	private static void diamond() {
-
+	/**
+	 * @param size
+	 */
+	private static void diamond(int size) {
+		System.out.println("###다이아몬드###");
+		int count = 0;
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				System.out.print((size / 2 - count <= col && col <= size / 2 + count) ? "*" : " ");
+			}
+			System.out.println();
+			if (row < (size / 2))
+				count++;
+			else
+				count--;
+		}
 	}
 
-	private static void Sandglass() {
-
+	/**
+	 * @param size
+	 */
+	private static void Sandglass(int size) {
+		System.out.println("###모래시계###");
+		int count = 0;
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				System.out.print((count <= col && col <= size - count - 1) ? "*" : " ");
+			}
+			System.out.println();
+			if (row < (size / 2))
+				count++;
+			else
+				count--;
+		}
 	}
 
-	private static void rectangle() {
-
+	/**
+	 * @param size
+	 */
+	private static void rectangle(int size) {
+		System.out.println("###직각 삼각형###");
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				System.out.print((row >= col) ? "*" : " ");
+			}
+			System.out.println();
+		}
 	}
-
 }
